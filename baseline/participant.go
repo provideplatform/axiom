5fa75bbf7737030012bcb82a8ad459e8eeab56a7
Kyle Thomas <kyle@provide.services>
2021-11-16 02:31:53 -0500

    Update participant foreign key references

--- baseline/participant.go
+++ baseline/participant.go
@@ -12,9 +12,10 @@ import (
 // Participant is a party to a baseline workgroup or workflow context
 type Participant struct {
 	baseline.Participant
-	Workgroups []*Workgroup `gorm:"many2many:workgroups_participants" json:"workgroups,omitempty"`
-	Workflows  []*Workflow  `gorm:"many2many:workflows_participants" json:"workflows,omitempty"`
-	Worksteps  []*Workstep  `gorm:"many2many:worksteps_participants" json:"worksteps,omitempty"`
+	Address    *string      `json:"address"`
+	Workgroups []*Workgroup `gorm:"many2many:workgroups_participants;References:address" json:"workgroups,omitempty"`
+	Workflows  []*Workflow  `gorm:"many2many:workflows_participants;References:address" json:"workflows,omitempty"`
+	Worksteps  []*Workstep  `gorm:"many2many:worksteps_participants;References:address" json:"worksteps,omitempty"`
 }
 
 func (p *Participant) Cache() error {
