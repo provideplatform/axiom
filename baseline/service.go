378f1a8fff723a3939ea1fe23b6f7912522f980c
Kyle Thomas <kyle@provide.services>
2021-11-24 02:31:44 -0500

    Fix nil pointer in outbound protocol message broadcast

--- baseline/service.go
+++ baseline/service.go
@@ -350,14 +350,18 @@ func (m *Message) baselineOutbound() bool {
 	common.Log.Debugf("dispatching outbound protocol message intended for %d recipients", len(recipients))
 
 	for _, recipient := range recipients {
-		common.Log.Debugf("dispatching outbound protocol message to %s", *recipient.Address)
-		err := m.ProtocolMessage.broadcast(*recipient.Address)
-		if err != nil {
-			msg := fmt.Sprintf("failed to dispatch protocol message to recipient: %s; %s", *recipient.Address, err.Error())
-			common.Log.Warning(msg)
-			m.Errors = append(m.Errors, &provide.Error{
-				Message: common.StringOrNil(msg),
-			})
+		if recipient.Address != nil {
+			common.Log.Debugf("dispatching outbound protocol message to %s", *recipient.Address)
+			err := m.ProtocolMessage.broadcast(*recipient.Address)
+			if err != nil {
+				msg := fmt.Sprintf("failed to dispatch protocol message to recipient: %s; %s", *recipient.Address, err.Error())
+				common.Log.Warning(msg)
+				m.Errors = append(m.Errors, &provide.Error{
+					Message: common.StringOrNil(msg),
+				})
+			}
+		} else {
+			common.Log.Warning("failed to dispatch protocol message to recipient; no recipient address")
 		}
 	}
 
