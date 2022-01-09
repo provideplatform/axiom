327f6b2cbc2733e8b69d0825c86263299177fb1b
Kyle Thomas <kyle@provide.services>
2022-01-09 03:24:28 -0500

    Set created_at timestamp on new versions

--- baseline/workflow.go
+++ baseline/workflow.go
@@ -704,10 +704,11 @@ func (w *Workflow) addVersion(version string, tx *gorm.DB) bool {
 	common.Log.Debugf("adding workflow version %s; workflow: %s", version, w.ID)
 	initialWorkflowID, _ := w.initialWorkflowVersion(tx)
 	var result *gorm.DB
+	createdAt := time.Now()
 	if initialWorkflowID != nil {
-		result = tx.Exec("INSERT INTO workflows_versions (initial_workflow_id, workflow_id, version) VALUES (?, ?, ?)", initialWorkflowID, w.ID, version)
+		result = tx.Exec("INSERT INTO workflows_versions (created_at, initial_workflow_id, workflow_id, version) VALUES (?, ?, ?)", createdAt, initialWorkflowID, w.ID, version)
 	} else {
-		result = tx.Exec("INSERT INTO workflows_versions (workflow_id, version) VALUES (?, ?)", w.ID, version)
+		result = tx.Exec("INSERT INTO workflows_versions (created_at, workflow_id, version) VALUES (?, ?)", createdAt, w.ID, version)
 	}
 	success := result.RowsAffected == 1
 	if success {
