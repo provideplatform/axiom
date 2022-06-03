2f4b8fdfc25124e02d096755360ea0f158ca6f74
Kyle Thomas <kyle@provide.services>
2022-06-02 22:30:28 -0400

    Fix +build flag

--- test/proxy_test.go
+++ test/proxy_test.go
@@ -1,3 +1,5 @@
+// +build unit
+
 /*
  * Copyright 2017-2022 Provide Technologies Inc.
  *
@@ -13,8 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-// +build unit
 
 package test
 
